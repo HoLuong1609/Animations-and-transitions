b4dc4a0fc739cef2b401d2858bb7dea75ce93b18
aloha0211 <holuong0211@gmail.com>
2017-06-05 16:32:08 +0700

    Listen for animation events

--- Interpolators/app/src/main/java/com/example/android/java/MainActivity.java
+++ Interpolators/app/src/main/java/com/example/android/java/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.java;
 
+import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "AnimationEvents";
 
     private ImageView imageView;
     private RelativeLayout canvas;
@@ -30,6 +34,33 @@ public class MainActivity extends AppCompatActivity {
         ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "y", 0, targetY)
                 .setDuration(1000);
         animator.setInterpolator(new BounceInterpolator());
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+
+            }
+        });
+        animator.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
         animator.start();
     }
 
